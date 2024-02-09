8a21f4367c9031fa28ed84a151aba820dbde5919
Renan <renan.fachin@hotmail.com>
2024-02-09 19:17:06 -0300

    chore: Configura alias para imports

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { SafeAreaView } from 'react-native'
 import { Slot } from "expo-router";
 import { Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold, useFonts } from '@expo-google-fonts/inter'
-import { Loading } from '../components/loading'
+import { Loading } from '@/components/loading'
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
