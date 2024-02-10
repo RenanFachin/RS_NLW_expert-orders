7329b148ee92ffdc3c43d28ee609d642762f72fa
Renan <renan.fachin@hotmail.com>
2024-02-09 22:19:56 -0300

    style: Cria componente de header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,11 @@
+import { Header } from '@/components/header'
 import { View, Text } from 'react-native'
 
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text-2xl">
-        Hello NLW
-      </Text>
+      <Header title='Faça seu pedido' cartQuantity={0} />
     </View>
   )
 }
